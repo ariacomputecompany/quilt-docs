a1b29706e4c896ec85239da81e6cd8575278fa8d
saint <saint.0x.saint@gmail.com>
2026-04-06 14:52:26 -0400

    Align links indent to match sidebar subpages

--- src/components/Sidebar/SidebarLinks.tsx
+++ src/components/Sidebar/SidebarLinks.tsx
@@ -18,8 +18,8 @@ const links: LinkItem[] = [
 
 export function SidebarLinks() {
   return (
-    <div className="mb-5 pl-4 pr-2">
-      <h5 className="app-sidebar-group-title font-semibold text-xs uppercase tracking-wider mb-2 opacity-50">
+    <div className="mb-5 pr-2">
+      <h5 className="app-sidebar-group-title font-semibold text-xs uppercase tracking-wider mb-2 opacity-50 pl-4">
         Links
       </h5>
       <div className="flex flex-col gap-0.5 ml-4 pl-2 border-l border-[var(--app-border)]">
